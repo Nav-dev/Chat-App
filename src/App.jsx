c5c85a6b8994737f4e0015d6814c05702ff47782
Nav-dev <sharmanav325@gmail.com>
2023-07-18 20:12:35 +0530

    scafflod the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import React from 'react';
 
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/signin">SignIn</Route>
-        <Route path="/">Home</Route>
-      </Switch>
-    </BrowserRouter>
-  );
+  return <div>Hello</div>
 }
 
 export default App;
