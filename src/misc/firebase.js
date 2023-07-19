874f32041e134c3e9b3d5d99b9850e9df3645deb
Nav-dev <sharmanav325@gmail.com>
2023-07-19 12:46:06 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+
+const config = {
+
+    apiKey: "AIzaSyCM362ByvqskrTRUqsysrY6oGNMz0MllQs",
+  
+    authDomain: "chat-app-3b8d2.firebaseapp.com",
+  
+    projectId: "chat-app-3b8d2",
+  
+    storageBucket: "chat-app-3b8d2.appspot.com",
+  
+    messagingSenderId: "256021056248",
+  
+    appId: "1:256021056248:web:14fc303b2b6dbbfedc090d"
+  
+  };
+
+const app = firebase.initializeApp(config);
+  
