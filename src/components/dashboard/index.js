9e01eb82d71410de7c9a4cb650c06401ff673db3
Nav-dev <sharmanav325@gmail.com>
2023-07-24 22:57:38 +0530

    updated nickname portion

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Drawer, Button, Divider} from 'rsuite';
+import { Drawer, Button, Divider, Alert} from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import EditableInput from './editableInput';
+import { database } from '../../misc/firebase';
 
 const Dashboard = ({ onSignOut}) => {
 
     const { profile } = useProfile();
-    const onSave = async newData => {
-        console.log(newData);
-    }
+
+        const onSave = async newData => {
+            const userNicknameRef = database.ref(`/profiles/${profile.uid}`).child('name');
+
+            try{
+                await userNicknameRef.set(newData);
+
+                Alert.success('Nickname has been updated', 4000);
+            
+            } catch(err){
+                Alert.error(err.message,4000);
+        }
+    };
 
   return(
      <>
