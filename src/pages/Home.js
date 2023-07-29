be77c604ac7cc5f76af2073afd9aa96d14729e63
Nav-dev <sharmanav325@gmail.com>
2023-07-29 13:20:10 +0530

    created rooms context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Col, Grid , Row } from 'rsuite';
 import Sidebar from '../components/Sidebar';
+import { RoomsProvider } from '../context/rooms.context';
 
 const Home = () => {
   return (
+    <RoomsProvider>
     <Grid fluid className='h-100'>
       <Row className='h-100'>
         <Col xs={24} md={8} className='h-100'>
@@ -11,6 +13,7 @@ const Home = () => {
         </Col>
       </Row>
     </Grid>
+    </RoomsProvider>
   )
 }
 
