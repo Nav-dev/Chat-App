f7618f74c5d2bc023ddbf89e4a16d8f886eadded
Nav-dev <sharmanav325@gmail.com>
2023-07-29 16:53:08 +0530

    displayed chat room

--- src/components/rooms/ChatRoomList.js
+++ src/components/rooms/ChatRoomList.js
@@ -1,23 +1,41 @@
 import React from 'react'
-import { Nav } from 'rsuite'
-import RoomItem from './RoomItem'
+import { Loader, Nav } from 'rsuite'
+import RoomItem from './RoomItem';
+import { useRooms } from '../../context/rooms.context';
+import { Link, useLocation } from 'react-router-dom';
 
 const ChatRoomList = ({ aboveElHeight }) => {
+
+  const rooms = useRooms(); 
+  const location = useLocation(); 
   return (
    <Nav
-   appearence="subtle"
-   vertical
-   reversed
-   className='overflow-y-scroll custom-scroll'
-   style={{
-    height: `calc(100% - ${aboveElHeight}px)`,
+    appearence="subtle"
+    vertical
+    reversed
+    className='overflow-y-scroll custom-scroll'
+    style={{
+      height: `calc(100% - ${aboveElHeight}px)`,
    }}
+   activeKey={location.pathname}
    >
-      <Nav.Item>
-        <RoomItem />
+    {!rooms && (
+      <Loader center vertical content="Loading" speed="slow" size="md" />
+    )}
+    {rooms && 
+    rooms.length > 0 &&
+    rooms.map(room =>(
+      <Nav.Item 
+      componentClass={ Link } 
+      to={`/chat/${room.id}`} 
+      key= {room.id}
+      eventKey={`/chat/${room.id}`}
+      >
+        <RoomItem room={room} />
       </Nav.Item>
-   </Nav>
-  )
-}
+  ))} 
+  </Nav>
+  );
+};
 
 export default ChatRoomList
--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import TimeAgo from 'timeago-react'
 
-const RoomItem = () => {
-  return <div>
+const RoomItem = ({ room }) => {
+    const { createdAt, name } = room;
+    
+  return (
+  <div>
     <div className='d-flex justify-content-between align-items-center'>
-        <h3 className='text-dissapear'>Room name</h3>
-        <TimeAgo datetime={new Date()} className='font-normal text-black-45' />
+        <h3 className='text-dissapear'>{name}</h3>
+        <TimeAgo 
+            datetime={new Date(createdAt)} 
+            className='font-normal text-black-45' />
     </div>
     <div className='d-flex align-items-center text-black-70'>
         <span>No messages yet... </span>
     </div>
   </div>
-  
+  )
 }
 
-export default RoomItem
+export default RoomItem;
